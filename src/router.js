fdc931ca223377ab8135040e57508c74f3d0a0df
Johsav <john@savelid.se>
2019-01-03 20:59:47 +0100

    base config

--- src/router.js
+++ src/router.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Great from './views/Great.vue'
-import One from './views/PortfolioOne.vue'
-import Two from './views/PortfolioTwo.vue'
-import Three from './views/PortfolioThree.vue'
 
 Vue.use(Router)
 
@@ -21,32 +16,10 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      component: About
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/great',
-      name: 'great',
-      component: Great
-    },
-    {
-      path: '/one',
-      name: 'one',
-      component: One
-    },
-    {
-      path: '/portfolio/two',
-      name: 'two',
-      component: Two
-    }  ,
-    {
-      path: '/portfolio/three',
-      name: 'three',
-      component: Three
-    }  
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
   ]
-  //, mode: 'history'
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,7 @@
 <template>
-  <div class="home main-content">
-    <div class="left-content">
-      <h1>OUTDOOR ADVENTURES</h1>
-      <p class="subtext">Exceting outdoor experinces in Sweden with Naturguiden</p>
-      <p class="subtext">We do Ice skating on natural ice, Kayak tours, Hiking, Cross country skiing and other simular adventures</p>
-
-    </div>  
-    <div class="right-content img1">
-      <button><router-link to="/one">Ice skating</router-link></button>
-       <router-link to="/one">Ice skating</router-link>
-
-    </div>  
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -25,15 +16,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-  .left-content  {
-    background: #a3e6a3
-  }
-  .img1 {
-    /*background: url('../assets/home.jpg'); */
-    background: #eb8181
-  }
-</style>
-
