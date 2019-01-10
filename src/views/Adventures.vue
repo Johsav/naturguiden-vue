fc0114f185c8d1da13d88b8f04676f972a35552e
Robin Savelid <robin.savelid@eurostep.com>
2019-01-10 22:12:44 +0100

    Adventures page now uses v-for

--- src/views/Adventures.vue
+++ src/views/Adventures.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
     <v-container fluid grid-list-md style="background-color: #00ff00" class="no-padding-bottom">
-      <!-- <v-tabs fixed-tabs grow v-for="item in tabLinks" :key="item.title">
-        <v-tab :to="item.link">{{ item.title }}</v-tab>
-      </v-tabs>-->
       <v-card>
         <v-tabs fixed-tabs grow>
-          <v-tab>Ice-skating</v-tab>
-          <v-tab>Kayak</v-tab>
-          <v-tab>Hiking</v-tab>
+          <v-tab v-for="item in packages" :key="item.link" :to="item.link" replace>{{item.title}}</v-tab>
         </v-tabs>
       </v-card>
     </v-container>
@@ -16,54 +11,16 @@
     <LeftSideColumn>
       <v-container fluid grid-list-md>
         <v-layout column>
-          <v-flex>
-            <v-card>
+          <v-flex v-for="item in packages" :key="item.link" >
+            <v-card :to="item.link">
               <v-img
                 class="white--text"
                 height="200px"
-                :src="require('../assets/images/1400/Skating_1413_3.jpg')"
-              ><v-card-title>Skating pack 1</v-card-title></v-img>
-              <v-card-text></v-card-text>
-            </v-card>
-          </v-flex>
-                    <v-flex>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                :src="require('../assets/images/1400/hiking_1413_1.jpg')"
-              ><v-card-title>Skating pack 1</v-card-title></v-img>
-              <v-card-text></v-card-text>
-            </v-card>
-          </v-flex>
-                    <v-flex>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                :src="require('../assets/images/1400/kayak_1413_3.jpg')"
-              ><v-card-title>Skating pack 1</v-card-title></v-img>
-              <v-card-text></v-card-text>
-            </v-card>
-          </v-flex>
-                    <v-flex>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                :src="require('../assets/images/1400/Skating_1413_1.jpg')"
-              ><v-card-title>Skating pack 1</v-card-title></v-img>
-              <v-card-text></v-card-text>
-            </v-card>
-          </v-flex>
-                    <v-flex>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                :src="require('../assets/images/1400/Skating_1413_3.jpg')"
-              ><v-card-title>Skating pack 1</v-card-title></v-img>
-              <v-card-text></v-card-text>
+                :src="getImgUrl(item.imageSrc)"
+              >
+                <v-card-title>{{item.title}}</v-card-title>
+              </v-img>
+              <v-card-text>{{item.text}}</v-card-text>
             </v-card>
           </v-flex>
         </v-layout>
@@ -79,13 +36,45 @@ export default {
   components: {
     LeftSideColumn
   },
+  methods: {
+    getImgUrl(img) {
+      return require("@/assets/images/" + img);
+    }
+  },
   data() {
     return {
       drawer: null,
-      tabLinks: [
-        { title: "Ice-skating", link: "/" },
-        { title: "Kayak", link: "/about" },
-        { title: "Hiking", link: "/calendar" }
+      packages: [
+        {
+          title: "Ice-skating",
+          link: "/",
+          imageSrc: "1400/Skating_1413_3.jpg",
+          text: `Eight adventurous days filled with mountain peak tours,
+          glacier hiking and magnificent nature! The details are not set,
+          we decide from day to day how the trip will develop. We bring our
+          own tents (which will be provided) and all the food we need. We are totally free
+          to experience the wild, outside all regular tracks and mountain lodges.`
+        },
+        {
+          title: "Kayak",
+          link: "/about",
+          imageSrc: "1400/kayak_1413_4.jpg",
+          text: `Eight adventurous days filled with mountain peak tours,
+          glacier hiking and magnificent nature! The details are not set,
+          we decide from day to day how the trip will develop. We bring our
+          own tents (which will be provided) and all the food we need. We are totally free
+          to experience the wild, outside all regular tracks and mountain lodges.`
+        },
+        {
+          title: "Hiking",
+          link: "/calendar",
+          imageSrc: "1400/hiking_1413_4.jpg",
+          text: `Eight adventurous days filled with mountain peak tours,
+          glacier hiking and magnificent nature! The details are not set,
+          we decide from day to day how the trip will develop. We bring our
+          own tents (which will be provided) and all the food we need. We are totally free
+          to experience the wild, outside all regular tracks and mountain lodges.`
+        }
       ]
     };
   }
