8117f6447678ec85d1e8d4e67017b873f9560c3e
Johsav <john@savelid.se>
2019-08-22 18:54:06 +0200

    Update skating and Contact us

--- src/views/Contactus.vue
+++ src/views/Contactus.vue
@@ -1,58 +1,204 @@
 <template>
-  <v-container fluid grid-list-md style="background-color: #00ff00" class="no-padding-bottom">
-      <v-card>
-        <v-tabs fixed-tabs grow>
-          <v-tab v-for="item in m_element" :key="item.link" :to="item.link" replace>{{item.title}}</v-tab>
-        </v-tabs>
-      </v-card>
-   
+  <v-container fluid grid-list-md class="no-padding-bottom">
+    <v-card >
+      <v-tabs fixed-tabs grow slider-color="primary">
+        <v-tab v-for="item in m_element" :key="item.link" :to="item.link" replace>{{item.title}}</v-tab>
+      </v-tabs>
+    </v-card>
+
+    <v-layout row wrap fill-height>
+
+      <v-flex md3 lg2 xl2 d-flex class="hidden-p-xs-only">
         
-            <v-layout row fill-height>
-              <v-flex md3 lg2  xl2 d-flex  class="hidden-p-xs-only">
-                <v-card>
-                  <v-img
-                    class="white--text"
-                    contain
-                    
-                    :src="getImgUrl('Logo_RGB.jpg')"
-                  ></v-img>
-                </v-card>
-              </v-flex>
-              <v-flex xs12 md9 lg10 sm12 xl10 d-flex>
-                <v-card  class="text-xs-left">
-                  <v-card-title primary-title>
-                      <h3 class="headline">Contact information</h3>
-                  </v-card-title>
-                  <v-card-text>
-                    <h3>Naturguiden Svenska Äventyr</h3>
-                 
-                    <p>Naturguiden</P>
-                    <p>Munkhagsgatan 120B</P>
-                    <p>587 25 LINKÖPING</P>
-                    <p>SWEDEN</P>
-                    <p></p>
-                    <p>+46 70 53 53 630</P>
-                    <p>info@naturguiden.se</P>
-                    
-                    <p></p>
-                    <p>www.naturguiden.com</P>
-                    <p>www.naturguiden.se</P>
+          <v-img class="top" contain :src="getImgUrl('Logo_RGB.jpg')"></v-img>
+        
+      </v-flex>
 
-                    <h3>Facebook</h3>
-                    <p>https://www.facebook.com/Naturguiden/</p>
-      
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-            </v-layout>
-      </v-container>
+      <v-flex xs12 md9 lg10 sm12 xl10 d-flex>
+        <v-flex>
+          <v-card class="text-xs-left">
+            <v-card-title primary-title>
+              <h3 class="headline">Contact information</h3>
+            </v-card-title>
+            <v-card-text class="display-1">
+              Naturguiden Svenska Äventyr
+            </v-card-text>
+            <v-card-text>
+              <p>Munkhagsgatan 120B<br />
+              587 25 LINKÖPING<br />
+              SWEDEN</p>
+              <p></p>
+              <p>+46 70 53 53 630<br />
+              info@naturguiden.se</p>
 
+              <p></p>
+              <p>www.naturguiden.com<br />
+              www.naturguiden.se</p>
+
+              <h3>Facebook</h3>
+              <p>https://www.facebook.com/Naturguiden/</p>
+            </v-card-text>
+          </v-card>
+        
+          <v-card class="mt-3">
+            <v-card-title primary-title>
+              <h3 class="headline">Contact form</h3>
+            </v-card-title>
+            <form class="px-3">
+              <v-text-field
+                v-model="name"
+                :error-messages="nameErrors"
+                label="Name"
+                required
+                @input="$v.name.$touch()"
+                @blur="$v.name.$touch()"
+              ></v-text-field>
+              <v-text-field
+                v-model="email"
+                :error-messages="emailErrors"
+                label="E-mail"
+                required
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
+              ></v-text-field>
+              <v-text-field
+                v-model="phone"
+                :error-messages="phoneErrors"
+                label="Phone"
+                
+                @change="$v.phone.$touch()"
+                @blur="$v.phone.$touch()"
+              ></v-text-field>
+              <v-text-field
+                v-model="message"
+                :error-messages="messageErrors"
+                label="Message"
+                required
+                @input="$v.message.$touch()"
+                @blur="$v.message.$touch()"
+              ></v-text-field>
+
+              <v-btn @click="submit">submit</v-btn>
+              <v-btn @click="clear">clear</v-btn>
+            </form>
+          </v-card>
+        </v-flex>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 
-export default {
+  import { validationMixin } from 'vuelidate'
+  import { required, maxLength, email } from 'vuelidate/lib/validators'
+
+  export default {
+    mixins: [validationMixin],
+
+    validations: {
+      name: { required, maxLength: maxLength(10) },
+      email: { required, email },
+      select: { required },
+      checkbox: {
+        checked (val) {
+          return val
+        }
+      }
+    },
+
+    /*data() {
+    return {
+      drawer: null,
+      m_element: [
+        {
+          title: "About us",
+          link: "/about"
+        },
+        { 
+          title: "Contact us", 
+          link: "/contact"
+        }
+      ]
+    };
+  }, */
+
+    data: function()  {
+      name: ''
+      email: ''
+      phone: ''
+      message: ''
+    
+     return {
+      drawer: null,
+      m_element: [
+        {
+          title: "About us",
+          link: "/about"
+        },
+        { 
+          title: "Contact us", 
+          link: "/contact"
+        }
+      ]
+     }
+    },
   
+
+    computed: {
+      checkboxErrors () {
+        const errors = []
+        if (!this.$v.checkbox.$dirty) return errors
+        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+        return errors
+      },
+      selectErrors () {
+        const errors = []
+        if (!this.$v.select.$dirty) return errors
+        !this.$v.select.required && errors.push('Item is required')
+        return errors
+      },
+      nameErrors () {
+        const errors = []
+        if (!this.$v.name.$dirty) return errors
+        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.name.required && errors.push('Name is required.')
+        return errors
+      },
+      emailErrors () {
+        const errors = []
+        if (!this.$v.email.$dirty) return errors
+        !this.$v.email.email && errors.push('Must be valid e-mail')
+        !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+      }
+    },
+
+    methods: {
+      submit () {
+        this.$v.$touch()
+      },
+      clear () {
+        this.$v.$reset()
+        this.name = ''
+        this.email = ''
+        this.select = null
+        this.checkbox = false
+      },
+      getImgUrl(img) {
+      return require("@/assets/images/" + img);
+    }
+    }
+  }
+
+
+
+
+
+
+
+
+/*export default {
   methods: {
     getImgUrl(img) {
       return require("@/assets/images/" + img);
@@ -67,12 +213,7 @@ export default {
           title: "About us",
           link: "/about"
         },
-        { title: "Contact us",
-          link: "/contact"
-        },
-        { title: "Send an inquiry",
-          link: "/inquiry"
-        }
+        { title: "Contact us", link: "/contact" }
       ],
       l_element: [
         {
@@ -80,22 +221,27 @@ export default {
           text: "It's fantastic and sometimes little scary.",
           link: "/skating"
         },
-        { title: "Kayak",
-         text: "All those islands in the archipelago, amazing.",
+        {
+          title: "Kayak",
+          text: "All those islands in the archipelago, amazing.",
           link: "/kayak"
         },
-        { title: "Hiking",
-         text: "Find the way and yourself up in the mountain.",
+        {
+          title: "Hiking",
+          text: "Find the way and yourself up in the mountain.",
           link: "/hiking"
         },
-        { title: "Back Country skiing",
-         text: "Spring in the mountain, they call it the fifth season. Still winter but with long lasting days.",
+        {
+          title: "Back Country skiing",
+          text:
+            "Spring in the mountain, they call it the fifth season. Still winter but with long lasting days.",
           link: "/skiing"
         }
       ]
-    }
-  }    
+    };
+  }
 };
+*/
 
 </script>
 
--- src/views/package/SkatingWeekend.vue
+++ src/views/package/SkatingWeekend.vue
@@ -1,102 +1,159 @@
 <template>
- <v-container fluid grid-list-md>
-    <v-layout row>
-      <v-flex lg10>
-        hello hello
-        <configured-carousel :carouselItems="carouselItems"></configured-carousel>
-      </v-flex>
-    </v-layout>
-
-    <v-layout row wrap grid-list-md>
-      <v-flex xs12 sm12 md8 lg7 xl7>
+  <AdventureMenu>
+    <v-layout row wrap grid-list-md class="pt-2">
+      <v-flex xs12 sm12 md9 lg9 xl9>
         <v-card>
-          <v-card-title>
-            <h1>Nordic Skating - the weekend tour for you</h1>
+          <v-tabs fixed-tabs grow slider-color="primary" color="grey lighten-1">
+            <v-tab
+              class="white--text"
+              v-for="item in type"
+              :key="item.link"
+              :to="item.link"
+              replace
+            >{{item.title}}</v-tab>
+          </v-tabs>
+        </v-card>
+        <v-flex>
+          <v-card class="grey lighten-3">
+            <v-card-title class="pt-1 pb-0">
+              <h1>Nordic Skating in Sweden</h1>
             </v-card-title>
-                
-                <v-card-text class="text-xs-left">
-                  <p>
-                  Nordic skating in Sweden, an all inclusive guided tour with a lot of good skating. We travel by minibus
-                  in small groups of maximum eight persons and one guide. Your guide picks you up at the airport/train
-                  station and if possible we go directly for skating on a good lake nearby. All needed equipment for
-                  skating is provided. We sleep in hostels and are making the dinner together in the evenings. At breakfast
-                  we also prepare our lunch that we bring in our backpacks.
-                  </p>
 
-                  <v-img
-                    class="right"
-                    width="100"                
-                    :src="require('@/assets/images/Logo_RGB.jpg')"
-                  >
-                  </v-img>
-                  <p>
-                  Where we skate depends on the ice and weather situation, we always choose the area that provides the
-                  best skating conditions. The accommodations are located in the area but could change from day to
-                  day depending on where we choose to go skating.
-                  </p>
-                  <p><i>
+            <v-card-text class="text-xs-left pt-1 subheading">
+              <v-img
+                contain
+                class="right"
+                width="100"
+                :src="require('@/assets/images/Logo_RGB.png')"
+              ></v-img>
+              <div>
+                It's great adventure, it's fun, it's skating on natural ice. Experience some amazing skating adventures in Sweden. The unspoiled countryside and the huge expanse of glossy ice – everything is here waiting for you.
+                Sweden is one of the best countries for Nordic skating. The variety of lakes and the Baltic sea gives us a lot of possibilities for ice-skating. No wonder that it have become very popular among all types of people in all ages.You don't need to be an expert on skates to come here for skating, but you need to be able to skate on some kind of skates, for example Rollerblades or figure skates.
+              </div>
+
+              <div>
+                We relay like to share this fantastic experience with you.
+                <b>Join us on our weekend tours or book your own private tour</b>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex>
+          <v-card>
+            <v-card-title>
+              <h1>Nordic Skating - the weekend tour for you</h1>
+            </v-card-title>
+
+            <v-card-text class="text-xs-left">
+              <p>Nordic skating in Sweden, an all inclusive guided tour with a lot of good skating. We travel by minibus in small groups of maximum eight persons and one guide. Your guide picks you up at the airport/train station and if possible we go directly for skating on a good lake nearby. All needed equipment for skating is provided. We sleep in hostels and are making the dinner together in the evenings. At breakfast we also prepare our lunch that we bring in our backpacks.</p>
+              <p>
+                Where we skate depends on the ice and weather situation, we always choose the area that provides the
+                best skating conditions. The accommodations are located in the area but could change from day to
+                day depending on where we choose to go skating.
+              </p>
+              <p>
+                <i>
                   If you come together with your friends or family and you are 4-6 persons or more consider the options
                   for your own private tour.
-                  <a href="/adventures/skating-yourown">Read more here..</a>
-                  </i></p>
-              
-                </v-card-text>
-              </v-card>
+                  <a
+                    href="/adventures/skating-yourown"
+                  >Read more here..</a>
+                </i>
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-flex>
       </v-flex>
-        
+
       <v-flex xl3 lg3 md4 sm12 xs12>
         <v-layout row wrap>
           <v-flex xl12 lg12 md12 sm6 xs12>
             <v-card>
-              <v-card-title class="d-block">
-                <h2 class="title">Nordic skating</h2>
-                <h3 class="subheading">weekend tour</h3>
+              <v-card-title class="d-block grey lighten-1">
+                <div class="title">Nordic skating</div>
+                <div class="subheading">weekend tour</div>
               </v-card-title>
 
-              <v-card-text class="text-xs-left">
-                <v-list-tile v-for="item in packageItems" :key="item.type">
-                  <v-list-tile-content>{{item.type}}</v-list-tile-content>
-                  <v-list-tile-content>{{item.content}}</v-list-tile-content>
-                </v-list-tile> 
+              <v-container fluid grid-list-sm>
+                <v-layout column>
+                  <v-flex v-for="item in packageItems" :key="item.type">
+                    <v-layout row>
+                      <v-card-text class="text-xs-right py-1 px-2">{{item.type}}</v-card-text>
+                      <v-card-text class="text-xs-left py-1 px-2">
+                       <v-html>{{item.content}}</v-html>
+                       </v-card-text>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-flex>
+
+          <v-flex xl12 lg12 md12 sm6 xs12>
+            <v-card>
+              <v-card-title class="d-block grey lighten-1">
+                <div class="title">Scheduled dates</div>
+                <div class="subheading">open groups 2020</div>
+              </v-card-title>
+
+              <v-container fluid grid-list-sm>
+                <v-layout column>
+                  <v-flex v-for="item in packageDates" :key="item.date">
+                    <v-layout row>
+                      <v-card-text class="text-xs-right py-1 px-2">{{item.date}}</v-card-text>
+                      <v-card-text class="text-xs-left py-1 px-2">
+                        {{item.level}}
+                      </v-card-text>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-flex>
+
+          <v-flex xl12 lg12 md12 sm6 xs12>
+            <v-card>
+              <v-card-title class="d-block grey lighten-1">BOOKNING</v-card-title>
+              <v-card-text>
+                <div id="bokun-w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad">Loading...</div>
               </v-card-text>
             </v-card>
-
           </v-flex>
-            
-            <v-flex xl12 lg12 md12 sm6 xs12>
-               <v-card >
-                <v-card-title primary class="title">What to bring</v-card-title>
-                <v-card-text>It's very important ....</v-card-text>
-              </v-card>
-              <v-card color="indigo" dark >  
-                <v-card-title primary class="title center">BOOKNING</v-card-title>
-                <v-card-text><div id="bokun-w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad">Loading...</div></v-card-text>
-              </v-card>
-               <v-card >
-                <v-card-title primary class="title">Tidningsklipp</v-card-title>
-                <v-card-text>artikel</v-card-text>
-              </v-card>
-          </v-flex>  
+          <v-flex xl12 lg12 md12 sm6 xs12>
+            <v-card>
+              <v-card-title class="d-block grey lighten-1">Important Information</v-card-title>
+              <v-card-text>It's very important ....</v-card-text>
+            </v-card>
+          </v-flex>
         </v-layout>
       </v-flex>
     </v-layout>
-  </v-container>
-    
-
+  </AdventureMenu>
 </template>
 
 <script>
-import ConfiguredCarousel from "@/components/ConfiguredCarousel.vue";
+import AdventureMenu from "@/components/AdventureMenu.vue";
+import { nbind } from 'q';
 
 export default {
   components: {
-    ConfiguredCarousel
+    AdventureMenu
   },
   data() {
     return {
+      type: [
+        {
+          title: "Skating weekend",
+          link: "/adventures/skating/weekend"
+        },
+        {
+          title: "Skating private tour",
+          link: "/adventures/skating/private"
+        }
+      ],
       carouselItems: [
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          src: require("@/assets/images/1400/Skating_1413_2.jpg"),
           title: "Ice-skating",
           subtitle: "Ice-skating is fun"
         },
@@ -116,16 +173,27 @@ export default {
           subtitle: "Ice-skating is fun"
         }
       ],
-       packageItems:  [
-        {type: "Season:", content: "January - mid March"},
-        {type: "Days:", content: "4 days/3 nights"},
-        {type: "Price:", content: "from 655 EUR"},
-        {type: "Meeting point:", content: "Arlanda airport"},
-        {type: "Lodging:", content: "Hostel in double room, shared toilet and shower"},
-
-             
+      packageItems: [
+        { type: "Season:", content: "January - mid March" },
+        { type: "Days:", content: "4 days/3 nights" },
+        { type: "Price:", content: "695 EUR \early season discount 37 EUR \r\nsingle room +75 EUR \nsheets \nand towel +25 EUR"},
+        { type: "Meeting point:", content: "Arlanda airport" },
+        {
+          type: "Lodging:",
+          content: "Hostel in double room, shared toilet and shower"
+        }
+      ],
+      packageDates: [
+        { date: "23 - 26 January:", level: "Weekend for normal skaters" },
+        { date: "30 Jan - 2 Febr:", level: "Weekend for good skaters" },
+        { date: "6 - 9 February:", level: "Weekend for normal skaters" },
+        { date: "13 - 16 February:", level: "Weekend for good skaters" },
+        {
+          date: "20 - 23 February:",
+          level: "Weekend for good and normal skaters - two groups"
+        },
+        { date: "27 Febr - 1 March:", level: "Weekend for normal skaters" }
       ]
-      
     };
   }
 };
@@ -133,16 +201,38 @@ export default {
 //Bokun script
 var w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad;
 (function(d, t) {
-  var host = 'widgets.bokun.io';
-  var frameUrl = 'https://' + host + '/widgets/32023?bookingChannelUUID=3ccd59a6-631f-4273-a87f-8899f0037f8f&amp;activityId=86253&amp;lang=en&amp;ccy=SEK&amp;hash=w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad';
-  var s = d.createElement(t), options = {'host': host, 'frameUrl': frameUrl, 'widgetHash':'w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad', 'autoResize':true,'height':'','width':'100%', 'minHeight': 0,'async':true, 'ssl':true, 'affiliateTrackingCode': '', 'transientSession': true, 'cookieLifetime': 43200 };
-  s.src = 'https://' + host + '/assets/javascripts/widgets/embedder.js';
+  var host = "widgets.bokun.io";
+  var frameUrl =
+    "https://" +
+    host +
+    "/widgets/32023?bookingChannelUUID=3ccd59a6-631f-4273-a87f-8899f0037f8f&amp;activityId=86253&amp;lang=en&amp;ccy=SEK&amp;hash=w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad";
+  var s = d.createElement(t),
+    options = {
+      host: host,
+      frameUrl: frameUrl,
+      widgetHash: "w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad",
+      autoResize: true,
+      height: "",
+      width: "100%",
+      minHeight: 0,
+      async: true,
+      ssl: true,
+      affiliateTrackingCode: "",
+      transientSession: true,
+      cookieLifetime: 43200
+    };
+  s.src = "https://" + host + "/assets/javascripts/widgets/embedder.js";
   s.onload = s.onreadystatechange = function() {
-    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+    var rs = this.readyState;
+    if (rs) if (rs != "complete") if (rs != "loaded") return;
     try {
-      w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad = new BokunWidgetEmbedder(); w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad.initialize(options); w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad.display();
+      w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad = new BokunWidgetEmbedder();
+      w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad.initialize(options);
+      w32023_a8213c1f_c7be_40bd_bf12_3597943dbaad.display();
     } catch (e) {}
   };
-  var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-})(document, 'script');
+  var scr = d.getElementsByTagName(t)[0],
+    par = scr.parentNode;
+  par.insertBefore(s, scr);
+})(document, "script");
 </script>
