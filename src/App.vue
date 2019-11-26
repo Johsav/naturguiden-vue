4f91d617e4e8e9805a269b2414bdc1b68c12a7aa
Johsav <john@savelid.se>
2019-11-26 14:38:13 +0100

    Making drop down sub menu

--- src/App.vue
+++ src/App.vue
@@ -33,19 +33,61 @@
       <router-link to="/">
         <v-img :src="require('./assets/name.png')" height="48" width="156" contain></v-img>
       </router-link>
-      
-  
-      <v-menu offset-y>
+
+<!-- Old menu without submenu      <v-menu offset-y>
         <v-btn flat slot="activator">
           <v-icon left>expand_more</v-icon>
           <span>Our Adventures</span>
         </v-btn>
         <v-list>
-      
           <v-list-tile v-for="item in dropdownLinks" :key="item.title" router :to="item.link">
-          <v-list-tile-title>{{item.title}}</v-list-tile-title>
+            <v-list-tile-title>{{item.title}}</v-list-tile-title>
           </v-list-tile>
+        </v-list>
+      </v-menu>
+-->
+      
+<!-- EGEN TEST ------------------------>
+      <v-menu offset-x>
+        <v-btn flat slot="activator">
+          <v-icon left>expand_more</v-icon>
+          <span>Our Adventures</span>
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <router-link to="/adventures">
+            <v-list-tile-title> All our adventures</v-list-tile-title>
+            </router-link>
+          </v-list-tile>
+        </v-list>
+        <v-list
+          v-for="item in items" 
+          :key="item.title"
+         >
 
+<!--<v-list
+          v-for="item in items" 
+          :key="item.title" 
+          v-model="item.active" 
+          no-action>   -->          
+
+          
+          <v-menu offset-x right open-on-hover>
+            <v-list-tile slot="activator">
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile>
+            <v-list dense>
+              <v-list-tile 
+                v-for="subItem in item.items"
+                :key="subItem.title"
+                @click
+                router :to="subItem.link"
+                v-model="item.active">
+
+                <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
         </v-list>
       </v-menu>
 
@@ -66,44 +108,51 @@
       </router-link>
     </v-toolbar>
 
-    <v-content app class="bg-red">  <!-- Innanför v-content ligger koden för de olika sidorna -->
+    <v-content app class="bg-red">
+      <!-- Innanför v-content ligger koden för de olika sidorna -->
       <router-view></router-view>
     </v-content>
-    <div>   <!-- Needed to avoid footer overwrite lower part of page -->
-    <v-footer dark app absolute height="auto" color="secondary">
-      <v-container fluid>
-        <v-layout align-center justify-start row fill-height wrap>
-          <v-flex xs6 sm4 md4 lg2 xl2>
-            <v-img
-                    class="ma-auto"
-                    width="100"
-                    :src="require('@/assets/images/Logo_RGB.png')"
-                  >
-                  </v-img>
-          </v-flex>        
-          <v-flex xs6 sm4 md4 lg2 xl2>Naturguiden<br />Munkhagsgatan 120B<br />587 25 LINKÖPING<br />SWEDEN</v-flex>
-          <v-flex xs6 sm4 md4 lg2 xl2>+46 70 53 53 630<br />info@naturguiden.com<br />www.naturguiden.com</v-flex>
-          <v-flex xs6 sm4 md4 lg2 xl2>
-            <router-link to="/about" class="white--text">About us</router-link><br />
-            <router-link to="/contact" class="white--text">Send an inquiry</router-link>
-          </v-flex>
-          <v-flex xs6 sm4 md4 lg2 xl2> 
-            <v-img
-                    class="ma-auto"
-                    width="100"           
-                    :src="require('@/assets/images/naturturismforetagen-logo.svg')">
-                    </v-img>
-          </v-flex>
-          <v-flex xs6 sm4 md4 lg2 xl2> 
-            <v-img
-                    class="ma-auto"
-                    width="100"           
-                    :src="require('@/assets/images/logga_upplevelser.svg')">
-                    </v-img>
-          </v-flex>                
-        </v-layout>
-      </v-container>
-    </v-footer>
+    <div>
+      <!-- Needed to avoid footer overwrite lower part of page -->
+      <v-footer dark app absolute height="auto" color="secondary">
+        <v-container fluid>
+          <v-layout align-center justify-start row fill-height wrap>
+            <v-flex xs6 sm4 md4 lg2 xl2>
+              <v-img class="ma-auto" width="100" :src="require('@/assets/images/Logo_RGB.png')"></v-img>
+            </v-flex>
+            <v-flex xs6 sm4 md4 lg2 xl2>
+              Naturguiden
+              <br />Munkhagsgatan 120B
+              <br />587 25 LINKÖPING
+              <br />SWEDEN
+            </v-flex>
+            <v-flex xs6 sm4 md4 lg2 xl2>
+              +46 70 53 53 630
+              <br />info@naturguiden.com
+              <br />www.naturguiden.com
+            </v-flex>
+            <v-flex xs6 sm4 md4 lg2 xl2>
+              <router-link to="/about" class="white--text">About us</router-link>
+              <br />
+              <router-link to="/contact" class="white--text">Send an inquiry</router-link>
+            </v-flex>
+            <v-flex xs6 sm4 md4 lg2 xl2>
+              <v-img
+                class="ma-auto"
+                width="100"
+                :src="require('@/assets/images/naturturismforetagen-logo.svg')"
+              ></v-img>
+            </v-flex>
+            <v-flex xs6 sm4 md4 lg2 xl2>
+              <v-img
+                class="ma-auto"
+                width="100"
+                :src="require('@/assets/images/logga_upplevelser.svg')"
+              ></v-img>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-footer>
     </div>
   </v-app>
 </template>
@@ -128,11 +177,46 @@ export default {
         { title: "Contact us", link: "/contact" }
       ],
       dropdownLinks: [
-        {title: "All our adventures", link: "/adventures/" },
-        {title: "Nordic skating", link: "/adventures/skating/weekend" },
-        {title: "Kayak", link: "/adventures/kayak/weekend" },
-        {title: "Hiking", link: "/adventures/hiking/weekend" },
-        {title: "Back country skiing", link: "/adventures/skiing/adventures" }
+        { title: "All our adventures", link: "/adventures/" },
+        {
+          title: "Nordic skating",
+          sublinks: [
+            { sub: "Open tour", link: "/adventures/skating/weekend" },
+            { sub: "Private tour", link: "/adventures/skating/private" }
+          ]
+        },
+        { title: "Nordic skating", link: "/adventures/skating/weekend" },
+        { title: "Kayak", link: "/adventures/kayak/weekend" },
+        { title: "Hiking", link: "/adventures/hiking/weekend" },
+        { title: "Back country skiing", link: "/adventures/skiing/adventures" }
+      ],
+
+      items: [
+        { title: "Nordic skating",
+          items: [
+            { title: "Open tour", link: "/adventures/skating/weekend" },
+            { title: "Private tour", link: "/adventures/skating/private" }
+          ]
+        },
+        { title: "Kayak",
+          items: [
+            { title: "Open tour", link: "/adventures/kayak/weekend" },
+            { title: "Private tour", link: "/adventures/kayak/private" }
+          ]
+        },
+        { title: "Hiking",
+          items: [
+            { title: "Open tour", link: "/adventures/hiking/eightdays" },
+            { title: "Private tour", link: "/adventures/hiking/private" }
+          ]
+        },
+        { title: "Cross country skiing",
+          items: [
+            { title: "Open tour", link: "/adventures/skiing/weekend" },
+            { title: "Private tour", link: "/adventures/skiing/private" },
+            { title: "Winter adventures", link: "/adventures/skiing/adventures" }
+          ]
+        }
       ],
       mini: false,
       right: null
